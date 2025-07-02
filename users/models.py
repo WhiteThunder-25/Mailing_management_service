a95d105127407327727323f9431dcecfa0204283
Marina <lav_mari@mail.ru>
2025-07-02 03:44:49 +0300

    3

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from phonenumber_field.modelfields import PhoneNumberField
 
 
 class User(AbstractUser):
-    username = models.EmailField(unique=True, null=True)
+    username = None
     email = models.EmailField(unique=True, verbose_name="Email", help_text="Введите email")
     token = models.CharField(max_length=100, verbose_name="Токен", blank=True, null=True)
     avatar = models.ImageField(
@@ -13,7 +13,7 @@ class User(AbstractUser):
     phone_number = PhoneNumberField(blank=True, verbose_name="Телефон", help_text="Введите номер телефона")
     country = models.CharField(max_length=50, blank=True, null=True, verbose_name="Страна", help_text="Введите страну")
 
-    USERNAME_FIELD = "username"
+    USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
     class Meta:
